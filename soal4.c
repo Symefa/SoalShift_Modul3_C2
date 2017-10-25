8cf023d7ff8a2b6964c17bf34613ef5e5ace23e4
Alifa Izzan Akhsani <izzan@mhs.if.its.ac.id>
2017-10-25 08:59:32 +0700

    fungsi faktorial

--- soal4.c
+++ soal4.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <pthread>
+
+
+int t[3];
+int status;
+pthread_t tid[3];
+
+void *faktorial(void *arg)
+{
+  int *x = (int *)arg;
+  int top=t[x], result=1;
+	while(top>0)
+  {
+	 result*=top;
+   top--;
+  }
+}
 
 int main()
 {

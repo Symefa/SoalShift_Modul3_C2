f8290ce610e88e52caa2792a3be999ac2dbd14b2
Alifa Izzan Akhsani <izzan@mhs.if.its.ac.id>
2017-10-25 09:28:06 +0700

    Init kepiting lohan

--- soal3.c
+++ soal3.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <pthread.h>
+
+int status = 1;
+
+int kepiting = 100;
+int signal1= 0;
+
+int lohan = 100;
+int signal2 = 0;
+
+void *kepitings(void * arg);
+void *lohans(void *arg);
 
 int main()
 {
 
 return 0;
+}
+
+void *kepitings(void * arg)
+{
+
+}
+
+void *lohans(void *arg)
+{
+
 }

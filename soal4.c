5670cdd54295109358a155114335773dedb33776
Alifa Izzan Akhsani <izzan@mhs.if.its.ac.id>
2017-10-25 09:09:16 +0700

    soal 4 done

--- soal4.c
+++ soal4.c
@@ -8,12 +8,35 @@ pthread_t tid[3];
 
 void *faktorial(void *arg)
 {
+  int top=t[0], result=1;
+	while(top>0)
+  {
+	 result*=top;
+   top--;
+  }
+  printf("Hasil %d! = %d\n", t[0], result);
+}
+
+void *faktoriall(void *arg)
+{
   int top=t[1], result=1;
 	while(top>0)
   {
 	 result*=top;
    top--;
   }
+  printf("Hasil %d! = %d\n", t[1], result);
+}
+
+void *faktorialll(void *arg)
+{
+  int top=t[2], result=1;
+	while(top>0)
+  {
+	 result*=top;
+   top--;
+  }
+  printf("Hasil %d! = %d\n", t[2], result);
 }
 
 int main()
@@ -21,10 +44,9 @@ int main()
 
   scanf("%d %d %d", &t[0],&t[1],&t[2]);
 
-  for (int i = 0; i<3; i++)
-  {
-    pthread_create(&(tid[0]),NULL,&faktorial,(void *)x[i]);
-  }
+    pthread_create(&(tid[0]),NULL,&faktorial,NULL);
+    pthread_create(&(tid[0]),NULL,&faktoriall,NULL);
+    pthread_create(&(tid[0]),NULL,&faktorialll,NULL);
   for (int i = 0; i<3; i++)
   {
     pthread_join(tid[i],NULL);

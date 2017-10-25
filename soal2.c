ee0b1be05b1948424faf34d915a72743e3fa0844
Alifa Izzan Akhsani <izzan@mhs.if.its.ac.id>
2017-10-25 10:21:45 +0700

    Reformat

--- soal2.c
+++ soal2.c
@@ -18,6 +18,24 @@ int score2=0;
 int filled2=0;
 char name2[30];
 
+void printscore();
+void *player1(void* arg);
+void *player2(void* arg);
+
+int main()
+{
+  pthread_t tid[2];
+ printf("masukkan nama1:\n");
+ scanf("%s", name1);
+ printf("masukkan nama2:\n");
+ scanf("%s", name2);
+ pthread_create(&tid[0],NULL,&player1,NULL);
+ pthread_create(&tid[1],NULL,&player2,NULL);
+ pthread_join(tid[0],NULL);
+ pthread_join(tid[1],NULL);
+return 0;
+}
+
 void printscore()
 {
   printf("score player %s: %d\n",name1, score1);
@@ -167,19 +185,4 @@ void *player2(void* arg)
     }
   }
   wait2=0;
-}
-
-
-int main()
-{
-  pthread_t tid[2];
- printf("masukkan nama1:\n");
- scanf("%s", name1);
- printf("masukkan nama2:\n");
- scanf("%s", name2);
- pthread_create(&tid[0],NULL,&player1,NULL);
- pthread_create(&tid[1],NULL,&player2,NULL);
- pthread_join(tid[0],NULL);
- pthread_join(tid[1],NULL);
-return 0;
 }
